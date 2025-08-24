cb84735b80fe7071bac5c877b741a0b469ef8416
Neko <neko@ayaka.moe>
2025-08-24 18:46:27 +0800

    feat(adapaters/event-emitter): now supports NodeJS.EventEmitter (#6)

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   entry: {
     'index': 'src/index.ts',
+    'adapters/event-emitter/index': 'src/adapters/event-emitter/index.ts',
     'adapters/event-target/index': 'src/adapters/event-target/index.ts',
     'adapters/webworkers/index': 'src/adapters/webworkers/index.ts',
     'adapters/webworkers/worker/index': 'src/adapters/webworkers/worker/index.ts',
